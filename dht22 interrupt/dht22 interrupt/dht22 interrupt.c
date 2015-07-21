2ad5e107bbeda5c81e4efe17ee1740a0b991fb7f
Carl Anderson <carl.anderson10@gmail.com>
2015-07-20 21:24:41 -0400

    add further code for temp measure
    it's not quite working right, but the interrupt is triggering properly
    and it's doing *something*, even if the temperature measurement is
    completely wrong (228F? I doubt it's that hot.)

--- dht22 interrupt/dht22 interrupt/dht22 interrupt.c
+++ dht22 interrupt/dht22 interrupt/dht22 interrupt.c
@@ -17,8 +17,8 @@ void init();
 void dht22_read();
 
  
-volatile uint16_t dht_humidity; // Bytes 0 & 1 of the received data divided by 10
-volatile uint16_t dht_temp; // Bytes 2 & 3 of received data divided by 10
+volatile uint16_t dht_humidity = 0xff; // Bytes 0 & 1 of the received data divided by 10
+volatile uint16_t dht_temperature = 0xff; // Bytes 2 & 3 of received data divided by 10
 
 int main(void)
 {
@@ -31,7 +31,7 @@ int main(void)
 	
     while(1)
     {
-		
+		PORTD = dht_temperature;
     }
 }
 
@@ -44,18 +44,29 @@ void init(){
 	TCCR1B |= (1 << WGM12); // Timer/Counter1 in clear timer on compare mode
 	TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
 	
-	// TODO: enable port B pins as output for binary LEDs. (future, use shift register maybe?);
+	sei(); // Re-enable interrupts!
+
 	DDRB |= (1 << SENSOR_PIN);
 	PORTB |= (1 << SENSOR_PIN); //Set Sensor pin as output, and pull it high.
 	
-	sei(); // Re-enable interrupts!
+	DDRD = 0xFF; //Set all PORT D pins as output. 
+	DDRC |= (1 << DDC5); 
 	
 }
 
-void dht22_read(){
+void dht22_read(){ 
 	uint8_t data[5]; // Five bytes for received data from DHT22 Sensor. 5th byte is checksum.
-	uint8_t rcv_byte; // Current byte being received. Will be added to the data[] array after loop.
+	/* 
+		- data[0] and data[1] -- relative humidity multiplied by ten
+		- data[2] and data[3] -- temperature in celsius multiplied by ten..
+		- data[4]			  -- check sum of previous data: data[0] + ... + data[3] = data[4]
+	*/
 	
+	uint8_t rcv_byte; // Current byte being received. Will be added to the data[] array after loop.
+	uint8_t sum; // Used for addition of data bytes 0 through 3 to observe against checksum.
+	//uint8_t to_cnt;
+	
+
 	/* INITIALIZING Sensor: */
 
 	//OUTPUT: low for 1-10ms, then high
@@ -70,27 +81,59 @@ void dht22_read(){
 	// INPUT: 
 	DDRB &= ~(1 << SENSOR_PIN); // set sensor pin as input.
 	_delay_us(40); // in middle of expected low from sensor: 
+
 	if(PINB0 & 1){
 		// something went wrong. flash leds maybe? I dunno.
-		return;
+		//return;
 	}
 	_delay_us(60) ; // should be in the middle of the high signal from sensor
 	if(PINB0 & 0){
 		// something went wrong. cry yourself to sleep
-		return;
+		//return;
 	}
+	
 	// Sensor then pulls low for 80us, then high for 80us.
-
+	_delay_us(160);
+	
 	/* FOR LOOPS read temperature and humidity date in from sensor. */
 	for (uint8_t b = 0; b < 5; b++) { // Reading a total of five bytes from the sensor
 		
 		rcv_byte = 0; 
 		
 		for(uint8_t i = 0; i < 8; i++){ // 8 bits.
-			// before each data bit, sensor drops low for 50us
-			_delay_us(50);		
+			
+			// this is the problematic rising edge
+			// datasheet: duration defines bit: 26-28us for 0, 70 for 1, so read at 30 + (70-30)/2 = 50
+			_delay_us(100);
+			
+			rcv_byte <<= 1;
+			if(PINB & (1 << SENSOR_PIN))			// read bit
+				rcv_byte |= 1;
 		}
+		
+		data[b] = rcv_byte; // Saves the byte just retrieved from the sensor to its appropriate position. (See datasheet).
 	}
+	
+	sum = data[0] + data[1] + data[2] + data[3];
+	if(data[4] != sum) { // Verify the sum of the data received against the checksum bits
+		
+		// cry some more because you failed.
+		// TODO: Add in some kind of error handling. Whether it's a simple LED or other.
+		
+	}
+	
+	dht_humidity = ((data[0] << 8) | data[1]) / 10; // Data received is 2 bytes wide, so we need to combine bytes and divide by ten.
+	dht_temperature = (((data[2] & 0b01111111) << 8) | data[3]) / 10; // Removing the sign bit and combining, then dividing by ten.
+	
+	if(data[2] & 0b10000000){
+		dht_temperature *= -1;
+	}
+	
+	dht_temperature = (dht_temperature * 1.8) + 32;
+
+	dht_humidity = floor(dht_humidity);
+	dht_temperature = floor(dht_temperature);
+
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -99,5 +142,6 @@ ISR(TIMER1_COMPA_vect) {
 			- Purpose: Perform a read on the DHT22 sensor. 
 			- Read function of sensor is called.
 		*/
+		PORTC ^= (1 << DDC5);
 		dht22_read();
 }
